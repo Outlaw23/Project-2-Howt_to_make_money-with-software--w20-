a2300d42227ad7b538abdd1d3f0b64b32f8b3b81
Outlaw23 <127243391+Outlaw23@users.noreply.github.com>
2025-01-30 10:14:57 +0100

    test mapje

--- test/test.py
+++ test/test.py
@@ -1,10 +1,20 @@
 import json
 
 data = {
-    "naam": "Jan",
-    "leeftijd": 30,
-    "hobby's": ["lezen", "voetbal", "muziek"]
+        "company": {
+        "name": "Paper Code",
+        "address": "Puzzelpap, 2367YU Antwerpen",
+        "phone": "+32 069876543",
+        "email": "PaperCode@outlook.com"
+        }
 }
 
 with open("data.json", "w") as json_file:
     json.dump(data, json_file, indent=4)
+
+    #     "company": {
+    #     "name": "Paper Code",
+    #     "address": "Puzzelpap, 2367YU Antwerpen",
+    #     "phone": "+32 069876543",
+    #     "email": "PaperCode@outlook.com"
+    #     }
